c526aa67a5d984e7ea48ac8cf0a9e47f402cbe20
Hotciv <victorbarbozamo@gmail.com>
2021-07-02 17:53:57 -0500

    refactor(datasets): read all use dtst of paper

--- Reading_datasets.py
+++ Reading_datasets.py
@@ -4,15 +4,20 @@ import numpy as np
 
 data = arff.loadarff('Datasets\PhishingData.arff')
 ds1 = pd.DataFrame(data[0], dtype=np.int8)
+# aux = ds1[ds1["Result"] == 0]
+# aux.loc["Result"] = 1
+ds1["Result"].replace({0: 1}, inplace=True)
 '''
     All attributes in range [-1, 1]
     Result: -1 - phishing
              0 - suspicious
              1 - legitimate
+
+    Shirazi et al. assumed the suspicious as legitimate
 '''
 
-# data = arff.loadarff('Datasets\TrainingDataset.arff')
-# ds2 = pd.DataFrame(data[0], dtype=np.int8)
+data = arff.loadarff('Datasets\TrainingDataset.arff')
+ds2 = pd.DataFrame(data[0], dtype=np.int8)
 '''
     All attributes in range [-1, 1]
     Result not clear, will adopt this convention:
@@ -20,8 +25,8 @@ ds1 = pd.DataFrame(data[0], dtype=np.int8)
              1 - legitimate
 '''
 
-# data = arff.loadarff('Datasets\h3cgnj8hft-1\Phishing_Legitimate_full.arff')
-# ds3 = pd.DataFrame(data[0], dtype=np.int8)
+data = arff.loadarff('Datasets\h3cgnj8hft-1\Phishing_Legitimate_full.arff')
+ds3 = pd.DataFrame(data[0], dtype=np.int8)
 '''
     Some attributes are > 1, some are between 0 and 1, inclusive
     CLASS_LABEL not clear, will adopt this convention: 

8fbb6e84b24819a27017b024611e7ef8be299ec1
Hotciv <victorbarbozamo@gmail.com>
2021-07-02 23:41:17 -0500

    refactor(datasets): change label and ds names

--- Reading_datasets.py
+++ Reading_datasets.py
@@ -2,11 +2,38 @@ from scipy.io import arff
 import pandas as pd
 import numpy as np
 
+'''
+    All classes will be relabeled as follows:
+    +1 -> phishing instances
+    -1 -> legitimate instances
+'''
+
+# ds1 = pd.read_csv('Datasets/72ptz43s9v-1/dataset_full.csv')
+# ds1_sub = pd.read_csv('Datasets/72ptz43s9v-1/dataset_small.csv')
+# ds1["Result"].replace({0: 1}, inplace=True)  # test column name
+'''
+    Some attributes are > 1, some are between 0 and 1, inclusive
+    phishing: 0 - legitimate
+              1 - phishing
+'''
+
+
+data = arff.loadarff('Datasets\TrainingDataset.arff')
+ds2 = pd.DataFrame(data[0], dtype=np.int8)
+ds2["Result"].replace({1: -1, -1: 1}, inplace=True)
+'''
+    All attributes in range [-1, 1]
+    Result not clear, will adopt this convention:
+            -1 - phishing
+             1 - legitimate
+'''
+
 data = arff.loadarff('Datasets\PhishingData.arff')
-ds1 = pd.DataFrame(data[0], dtype=np.int8)
+ds3 = pd.DataFrame(data[0], dtype=np.int8)
 # aux = ds1[ds1["Result"] == 0]
 # aux.loc["Result"] = 1
-ds1["Result"].replace({0: 1}, inplace=True)
+ds3["Result"].replace({0: 1}, inplace=True)
+ds3["Result"].replace({1: -1, -1: 1}, inplace=True)
 '''
     All attributes in range [-1, 1]
     Result: -1 - phishing
@@ -16,30 +43,14 @@ ds1["Result"].replace({0: 1}, inplace=True)
     Shirazi et al. assumed the suspicious as legitimate
 '''
 
-data = arff.loadarff('Datasets\TrainingDataset.arff')
-ds2 = pd.DataFrame(data[0], dtype=np.int8)
-'''
-    All attributes in range [-1, 1]
-    Result not clear, will adopt this convention:
-            -1 - phishing
-             1 - legitimate
-'''
-
 data = arff.loadarff('Datasets\h3cgnj8hft-1\Phishing_Legitimate_full.arff')
-ds3 = pd.DataFrame(data[0], dtype=np.int8)
+ds4 = pd.DataFrame(data[0], dtype=np.int8)
+ds4["CLASS_LABEL"].replace({0: 1, 1: -1}, inplace=True)
 '''
     Some attributes are > 1, some are between 0 and 1, inclusive
     CLASS_LABEL not clear, will adopt this convention: 
              0 - phishing
              1 - legitimate
-'''
-
-# ds4 = pd.read_csv('Datasets/72ptz43s9v-1/dataset_full.csv')
-# ds4_sub = pd.read_csv('Datasets/72ptz43s9v-1/dataset_small.csv')
-'''
-    Some attributes are > 1, some are between 0 and 1, inclusive
-    phishing: 0 - legitimate
-              1 - phishing
 '''
 
 '''

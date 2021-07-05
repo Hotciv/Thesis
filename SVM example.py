e94b128188489a4247cf9cb47c220261fe8820ea
Hotciv <victorbarbozamo@gmail.com>
2021-07-05 01:08:48 -0500

    feat(SVM example): add kernel

--- SVM example.py
+++ SVM example.py
@@ -1,8 +1,29 @@
+import pandas as pd
 import cvxpy as cp
+
+def LinearKernel(X: pd.DataFrame):
+    '''
+        returns: X' * X
+                An NxN matrix, where N is the number of features
+    '''
+    return X.dot(X.T)
+
+def GaussianKernel1(X):
+    '''
+        As extracted from MATLAB:
+        
+        % Vectorized RBF Kernel
+        % This is equivalent to computing the kernel on every pair of examples
+        X2 = sum(X.^2, 2);
+        K = bsxfun(@plus, X2, bsxfun(@plus, X2', - 2 * (X * X')));
+        K = kernelFunction(1, 0) .^ K;
+    '''
+    pass
 
 def train_svr(X,y):
     # Where did this kernel come from?
     # X = GaussianKernel1(X)
+    X = LinearKernel(X)
     n_features = X.shape[0]
     w = cp.Variable(n_features)
     b = cp.Variable(1)

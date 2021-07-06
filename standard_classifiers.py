4d034a36f9d3300f97cd65fec9a58cc3e4aadf24
Hotciv <victorbarbozamo@gmail.com>
2021-07-06 15:44:32 -0500

    refactor(classifiers): remove comments

--- standard_classifiers.py
+++ standard_classifiers.py
@@ -1,39 +1,27 @@
 # Code source: Gaël Varoquaux
 #              Andreas Müller
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
 # Modified for documentation by Jaques Grobler
 # Modified for use by Victor Barboza
 # License: BSD 3 clause
 
-# from sklearn.datasets import make_moons, make_circles, make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
-# from sklearn.preprocessing import StandardScaler
-# from matplotlib.colors import ListedColormap
-# import matplotlib.pyplot as plt
 from reading_datasets import *
 from csv import writer
+from time import time
 import numpy as np
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.gaussian_process.kernels import RBF
-# from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier  # , AdaBoostClassifier
-# from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
-# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import GradientBoostingClassifier
 
-# TODO:
-# Make them work with my datasets
-
-h = .02  # step size in the mesh
-
-# names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-#          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-#          "Naive Bayes", "QDA", "Gradient Boost"]
 names = ["Nearest Neighbors", "Linear SVM", "RBF SVM",
          "Decision Tree", "Random Forest", 
          "Naive Bayes", "Gradient Boost"]
@@ -42,19 +30,17 @@ classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
     SVC(gamma=2, C=1),
-    # GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    # MLPClassifier(alpha=1, max_iter=1000),
-    # AdaBoostClassifier(),
     GaussianNB(),
-    # QuadraticDiscriminantAnalysis(),
     GradientBoostingClassifier()]
 
 # Loading datasets
 # X - list of list of features
 # y - list of classes
-dataframes = [ds2, ds3, ds4]
+# dataframes = [ds1_sub, ds2, ds3, ds4]
+# dataframes = [ds2, ds3, ds4]
+dataframes = [ds1_sub]
 datasets = []
 for ds in dataframes:
     # TODO: reserve 200 phishing for generating adversarial samples
@@ -62,126 +48,45 @@ for ds in dataframes:
     y = y.values.tolist()
     X = ds.values.tolist()
     datasets.append((X, y))
-# Creating datasets
-# X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
-#                            random_state=1, n_clusters_per_class=1)
-# rng = np.random.RandomState(2)
-# X += 2 * rng.uniform(size=X.shape)
-# linearly_separable = (X, y)
-
-# datasets = [make_moons(noise=0.3, random_state=0),
-#             make_circles(noise=0.2, factor=0.5, random_state=1),
-#             linearly_separable
-#             ]
-
-# no plotting for now
-# figure = plt.figure(figsize=(27, 9))
-# i = 1
 
 # saving the results on a csv file
 f = open('standard_classifiers_results.csv', 'w')
 wrt = writer(f)
-# TODO: repeat the experiment 10x, average results and get standard deviations
-header = ['Dataset', 'Classifier', 'ACC', 'TPR', 'f1']
+header = ['Dataset', 'Classifier', 'ACC', 'TPR', 'f1', 'Time to execute']
 wrt.writerow(header)
 
-# iterate over datasets
-for ds_cnt, ds in enumerate(datasets):
-    # preprocess dataset, split into training and test part
-    X, y = ds
+# TODO: average results and get standard deviations from files
+# repeat experiment 10x
+for k in range(10):
 
-    # no scalling for now
-    # X = StandardScaler().fit_transform(X)
+    # iterate over datasets
+    for ds_cnt, ds in enumerate(datasets):
 
-    # TODO: is this really used?
-    # X_train, X_test, y_train, y_test = \
-    #     train_test_split(X, y, test_size=.4, random_state=42)
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=.4)
+        # TODO: preprocess dataset, split into training and test part?
+        X, y = ds
 
-    # no plotting for now
-    ################################################################
-    ########################### PLOTTING ###########################
-    ################################################################
-    # plotting prep
-    # x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    # y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    # xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-    #                      np.arange(y_min, y_max, h))
+        # no scalling for now
+        # X = StandardScaler().fit_transform(X)
 
-    # # just plot the dataset first
-    # cm = plt.cm.RdBu
-    # cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    # ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    # if ds_cnt == 0:
-    #     ax.set_title("Input data")
-    # # Plot the training points
-    # ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-    #            edgecolors='k')
-    # # Plot the testing points
-    # ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
-    #            edgecolors='k')
-    # ax.set_xlim(xx.min(), xx.max())
-    # ax.set_ylim(yy.min(), yy.max())
-    # ax.set_xticks(())
-    # ax.set_yticks(())
-    # i += 1
-    ################################################################
-    ########################### PLOTTING ###########################
-    ################################################################
+        # TODO: is this really used?
+        # X_train, X_test, y_train, y_test = \
+        #     train_test_split(X, y, test_size=.4, random_state=42)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=.96, random_state=42)
+        # X_train, y_train= (X, y)
 
-    print("\n\nGoing through DS"+str(ds_cnt + 1))
+        print("\n\nGoing through DS" + str(ds_cnt + 1) + ' ' + str(k + 1) + ' time')
 
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        # ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-        clf.fit(X_train, y_train)
-        # score = clf.score(X_test, y_test)
-        ACCs = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'accuracy')
-        f1s = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'f1')
-        TPRs = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'recall')
+        # iterate over classifiers
+        for name, clf in zip(names, classifiers):
+            # clf.fit(X_train, y_train)
+            # score = clf.score(X_test, y_test)
+            start_time = time()
+            ACCs = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'accuracy')
+            f1s = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'f1')
+            TPRs = cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'recall')
 
-
-        # no plotting for now
-        ################################################################
-        ########################### PLOTTING ###########################
-        ################################################################
-        # # Plot the decision boundary. For that, we will assign a color to each
-        # # point in the mesh [x_min, x_max]x[y_min, y_max].
-        # if hasattr(clf, "decision_function"):
-        #     Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        # else:
-        #     Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-
-        # # Put the result into a color plot
-        # Z = Z.reshape(xx.shape)
-        # ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-        # # Plot the training points
-        # ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-        #            edgecolors='k')
-        # # Plot the testing points
-        # ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-        #            edgecolors='k', alpha=0.6)
-
-        # ax.set_xlim(xx.min(), xx.max())
-        # ax.set_ylim(yy.min(), yy.max())
-        # ax.set_xticks(())
-        # ax.set_yticks(())
-        # if ds_cnt == 0:
-        #     ax.set_title(name)
-        # ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-        #         size=15, horizontalalignment='right')
-        # i += 1
-        ################################################################
-        ########################### PLOTTING ###########################
-        ################################################################
-
-        # header = ['Dataset', 'Classifier', 'ACC', 'TPR', 'f1']
-        wrt.writerow([ds_cnt, name, ACCs.mean(), TPRs.mean(), f1s.mean()])
-
-# no plotting for now
-# plt.tight_layout()
-# plt.show()
+            # header = ['Dataset', 'Classifier', 'ACC', 'TPR', 'f1', 'Time to execute']
+            wrt.writerow([ds_cnt, name, ACCs.mean(), TPRs.mean(), f1s.mean(), time() - start_time])
 
 f.close()

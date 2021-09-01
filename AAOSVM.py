a2d60981905fa19eae9732df2300530524100ddc
Hotciv <victorbarbozamo@gmail.com>
2021-09-01 09:48:48 -0500

    feat(AAOSVM): change init and Psi

--- AAOSVM.py
+++ AAOSVM.py
@@ -12,28 +12,13 @@ class AAOSVM(SMOModel):
     '''
     def __init__(self, X, y, C, alphas, b, errors, m=100, Gp=250, Em=1, Er=1, Ym=1, Yr=1, s=0.6, kernel_type='linear'):
         # Variables related to SVM with SMO
-        self.X = X                                      # training data matrix
-        self.y = y                                      # class label vector (-1 or 1) for the data
-        self.C = C                                      # regularization parameter
-        self.kernels = {                                # kernel types
-            'linear' : self.linear_kernel,
-            'gaussian' : self.gaussian_kernel
-        }
-        self.kernel = self.kernels[kernel_type]         # kernel function
-        self.alphas = alphas                            # lagrange multiplier vector
-        self.b = b                                      # scalar bias term
-        self.errors = errors                            # error cache
-        self._obj = []                                  # record of objective function value
-        # self.m = len(self.X)                            # store size of training set
-        self.w = np.zeros((len(X[0]),))                 # weight vector
+        super().__init__(X, y, C, alphas, b, errors, kernel_type)
+        if len(X) > 0:
+            self.w = np.zeros((len(X[0]),))             # weight vector
         
         # Variable(s) related to "relaxing" the SVM
         # self.slack = np.ones((len(X[0]),)) * s        # slack vector
         self.slack = s                                  # slack variable
-        
-        # Set tolerances
-        self.tol = tol                                  # error tolerance
-        self.eps = eps                                  # alpha tolerance
         
         # Game theory variables
         self.mem = m                                    # memory parameter
@@ -42,6 +27,7 @@ class AAOSVM(SMOModel):
         self.Er = Er                                    # reward for classifying regular correctly
         self.Ym = Ym                                    # punishment for misclassifying malicious
         self.Yr = Yr                                    # punishment for misclassifying regular
+        self.Psi = 0                                    # last Psi value
 
 
     def psi(self, x):
@@ -55,7 +41,7 @@ class AAOSVM(SMOModel):
         '''
         Function that incorporates prior knowledge into the SVM
         '''
-        return (1 + psi(x))/(self.w.T * np.ones((len(self.X[0]),)) + 2 * self.b)
+        self.Psi = (1 + psi(x))/(self.w.T * np.ones((len(self.X[0]),)) + 2 * self.b)
 
 
     def update_weights(self, i1, i2, a1, a2):

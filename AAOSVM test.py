1191a85a2191424a11f43facc0cff1ff93b2edea
Hotciv <victorbarbozamo@gmail.com>
2021-09-13 12:31:17 -0500

    feat(AAOSVM): add another outlier and partial_fit

--- AAOSVM test.py
+++ AAOSVM test.py
@@ -1,14 +1,12 @@
 from reading_datasets import *
 from AAOSVM import *
-from time import time
 
 # dataframes = [ds1_sub, ds2, ds3, ds4]
 # dataframes = [ds2, ds3, ds4]
 # dataframes = [ds3, ds4]
 dataframes = []
 
-X_train, y = make_blobs(n_samples=1000, centers=2,
-                        n_features=2, random_state=1)
+X_train, y = make_blobs(n_samples=1000, centers=2, n_features=2, random_state=1)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train, y)
@@ -17,8 +15,16 @@ y[y == 0] = -1
 
 # Add an outlier
 X_outlier = np.append(X_train_scaled, [0.1, 0.1])
-X_outlier = X_outlier.reshape(X_train.shape[0]+1, X_train.shape[1])
+X_outlier = X_outlier.reshape(X_train.shape[0] + 1, X_train.shape[1])
 y_outlier = np.append(y, 1)
+
+# Add a second outlier, so the first one is showed
+X_outlier = np.append(X_outlier, [0.1, 0.1])
+X_outlier = X_outlier.reshape(X_train.shape[0] + 2, X_train.shape[1])
+y_outlier = np.append(y_outlier, 0)
+
+# print(X_outlier)
+# print(y_outlier)
 
 datasets = []
 for ds in dataframes:
@@ -28,14 +34,16 @@ for ds in dataframes:
     x = ds.to_numpy()
     datasets.append((x, y))
 
+
 def calc_acc(y, y_hat):
     idx = np.where(y_hat == 1)
     TP = np.sum(y_hat[idx] == y[idx])
     idx = np.where(y_hat == -1)
     TN = np.sum(y_hat[idx] == y[idx])
-    return float(TP + TN)/len(y)
+    return float(TP + TN) / len(y)
 
-# Adding mock datasets    
+
+# Adding mock datasets
 datasets.append((X_train_scaled, y))
 datasets.append((X_outlier, y_outlier))
 
@@ -44,73 +52,51 @@ np.random.seed(0)
 # iterate over datasets
 for ds_cnt, ds in enumerate(datasets):
     print("\n\nGoing through DS" + str(ds_cnt + 1))
-    
+
     # Set model parameters and initial values
     C = 100.0
-    C = 1.0  # for SVM_w_SMO comparison
-    # m = len(ds[1])
+    # C = 1000.0  # for SVM_w_SMO comparison
+    # C = 1.0  # for SVM_w_SMO comparison
+    # m = len(ds[1])  # for SVM_w_SMO comparison
     m = 2
     initial_alphas = np.zeros(m)
     initial_b = 0.0
     initial_w = np.zeros(len(ds[0][0]))
     initial_Psi = 0.0
 
+    # # Initialize model
+    # model = AAOSVM(ds[0], ds[1], C,
+    #             initial_alphas, initial_b, np.zeros(m))
+    # model.w = initial_w
+
+    # # Initialize error cache
+    # initial_error = model.decision_function(model.X) - model.y
+    # model.errors = initial_error
+
+    # model.train()
+    ################################################################
     # Slidding window
     Sx = ds[0][0]
     Sy = ds[1][0]
     Sx = np.append([Sx], [ds[0][1]], 0)
     Sy = np.append([Sy], [ds[1][1]], 0)
-    
+
     # Initialize model
-    model = AAOSVM(Sx, Sy, C,
-                initial_alphas, initial_b, np.zeros(m))
+    model = AAOSVM(Sx, Sy, C, initial_alphas, initial_b, np.zeros(m))
     model.w = initial_w
 
     # X = ds[0]
-    # Y = ds[1] 
+    # Y = ds[1]
     for i in range(2, len(ds[1]), 1):
         x = ds[0][i]
         y = ds[1][i]
 
-        # if the instance is considered phishing
-        # if y * (initial_w @ x + initial_b) * initial_Psi < model.slack:
-        if y * (initial_w @ x + initial_b) < model.slack:
-            model.X = Sx
-            model.y = Sy
-            model.m = m
+        # if i == len(ds[1]) - 1 or i == len(ds[1]) - 2:
+        #     print(x, y)
+        # model.predi
+        Sx, Sy = model.partial_fit(Sx, Sy, x, y, i)
 
-            # Initialize error cache
-            initial_error = model.decision_function(model.X) - model.y
-            model.errors = initial_error
-
-            # Track time to train
-            tic = time()
-            # Train model
-            model.train()
-            toc = time()
-
-            initial_Psi = model.Psi
-            initial_b = model.b
-            initial_w = model.w
-
-        # if reached the limit of the window
-        # i.e. window is full and now will move
-        if Sy.shape[0] >= model.mem:
-            Sx = Sx[1:]
-            Sy = Sy[1:]
-
-        # Adding instance to the window
-        Sx = np.append(Sx, [x], 0)
-        Sy = np.append(Sy, [y], 0)
-
-        # Increasing the size of the parameters of the model
-        if model.mem > m:
-            m += 1
-            model.alphas = np.append(model.alphas, [0], 0)
-            model.errors = np.append(model.errors, [0], 0)
-            # print(len(Sx), len(Sy), len(model.alphas), len(model.errors))
-
-        # print('reached final {}'.format(i))
+        print("reached final {}".format(i))
 
     # # Support vector count
     # sv_count = np.where(model.alphas > 0)[0]
@@ -129,13 +115,11 @@ for ds_cnt, ds in enumerate(datasets):
     # print("accuracy:\t%.3f" % (acc))
     # print("Converged after %d iterations" % (iterations))
     # print("Time to train:\t\t" + str(toc - tic))
+    print(model.alphas.sum())
+
     fig, ax = plt.subplots()
     grid, ax = model.plot_decision_boundary(ax)
 
-    print(np.count_nonzero(model.alphas.nonzero()))
-    print(model.alphas.sum())
-    # input()
-    
 ################################################################
 ################# Test on mock dataset #########################
 ################################################################
@@ -167,7 +151,7 @@ for ds_cnt, ds in enumerate(datasets):
 # fig, ax = plt.subplots()
 # grid, ax = model.plot_decision_boundary(ax)
 
-# print(model.alphas.sum())  
+# print(model.alphas.sum())
 ################################################################
 ################# Test on mock dataset #########################
 ################################################################

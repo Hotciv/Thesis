ad888beb7066acaec54c0c75794bf008297f2cf9
Hotciv <victorbarbozamo@gmail.com>
2022-03-02 03:28:41 -0600

    refactor(adversarial_samples): formatter

--- adversarial_samples.py
+++ adversarial_samples.py
@@ -1,10 +1,12 @@
 from itertools import product
 from reading_datasets import *
+
 # import pandas as pd
 
 
 def generating_adversarial_samples(x, selFeatures, df, y=1):
-    '''Generates new samples based on previous ones and selected features
+    """
+    Generates new samples based on previous ones and selected features
     An important note is that it should be able to deal with samples that got successfully classified as phishing
 
 
@@ -13,7 +15,7 @@ def generating_adversarial_samples(x, selFeatures, df, y=1):
     selFeatures -- a list of indexes of the selected features
     df -- a dataframe that contains the dataset, with labels
     y -- which samples should be used (+1/legitimate or -1/phishing)
-    '''
+    """
     L = pd.DataFrame()
     L_unique = []
     genSamples = []
@@ -30,16 +32,16 @@ def generating_adversarial_samples(x, selFeatures, df, y=1):
         L_unique.append(L[col].unique())
 
     # Product possible values to generate all combinations
-    '''
+    """
     1 2  ->  1 2 | 3 2 | 3 4 | 1 4  
     3 4      3 4 | 1 4 | 1 2 | 3 2
 
     L_pr -- a list of lists, each sublist is a set of values that will be used to generate a new adversarial sample
-    '''
+    """
     # test = [[1, 3, 5], [2, 4, 6], [10, 20, 30]]
 
     # print(list(product(*test)))
-    
+
     # L_pr = list(product(*test))
     L_pr = list(product(*L_unique))
 
@@ -47,22 +49,23 @@ def generating_adversarial_samples(x, selFeatures, df, y=1):
     for sample in L_pr:
         temp = x.copy()
         i = 0
-        
+
         for j in selFeatures:
             temp[j] = sample[i]
             i += 1
-        
+
         genSamples.append(temp)
 
     return np.array(genSamples)
 
 
 def cost(a, b):
-    '''
+    """
     Euclidean distance between two pandas.Series or np.arrays
-    '''
+    """
 
-    return (np.linalg.norm(a-b))
+    return np.linalg.norm(a - b)
+
 
 # print(ds1.head())
 # print(generating_adversarial_samples(ds1.iloc[0, :], [0,2,4], ds1))

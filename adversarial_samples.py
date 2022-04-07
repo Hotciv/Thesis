b3663fd7d2f450720b7e59376e8c5f828ba5e15c
Hotciv <victorbarbozamo@gmail.com>
2022-04-07 17:09:53 -0500

    doc(adv_smpl): rm comments

--- adversarial_samples.py
+++ adversarial_samples.py
@@ -3,61 +3,6 @@ from itertools import product
 import numpy as np
 
 
-# def generating_adversarial_samples(x, selFeatures, df, y=1):
-#     """
-#     Generates new samples based on previous ones and selected features
-#     An important note is that it should be able to deal with samples that got successfully classified as phishing
-
-
-#     Keyword arguments:
-#     x -- an instance (a row)
-#     selFeatures -- a list of indexes of the selected features
-#     df -- a dataframe that contains the dataset, with labels
-#     y -- which samples should be used (+1/legitimate or -1/phishing)
-#     """
-#     L = pd.DataFrame()
-#     L_unique = []
-#     gen_samples = []
-
-#     # Gets only the legitimate or phishing samples
-#     df = df[df.iloc[:, -1] == y]
-
-#     # Gets the selected features from the dataset
-#     for featurePos in selFeatures:
-#         L[df.iloc[:, featurePos].name] = df.iloc[:, featurePos].copy()
-
-#     # Gets the unique values from the selected features
-#     for col in L:
-#         L_unique.append(L[col].unique())
-
-#     # Product possible values to generate all combinations
-#     """
-#     1 2  ->  1 2 | 3 2 | 3 4 | 1 4
-#     3 4      3 4 | 1 4 | 1 2 | 3 2
-
-#     L_pr -- a list of lists, each sublist is a set of values that will be used to generate a new adversarial sample
-#     """
-#     # test = [[1, 3, 5], [2, 4, 6], [10, 20, 30]]
-
-#     # print(list(product(*test)))
-
-#     # L_pr = list(product(*test))
-#     L_pr = list(product(*L_unique))
-
-#     # Generates new samples from the combinations
-#     for sample in L_pr:
-#         temp = x.copy()
-#         i = 0
-
-#         for j in selFeatures:
-#             temp[j] = sample[i]
-#             i += 1
-
-#         gen_samples.append(temp)
-
-#     return np.array(gen_samples)
-
-
 def generating_labels(gen_samples: np.ndarray, criteria: int, y=1)->np.array:
     """
     Generates labels for the gen_samples based on 'criteria'.
